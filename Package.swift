1c56b005430cb68582c8e2a5bb291cadc50ed495
Nikolay Petrov <nikolay.a.petrov@gmail.com>
2024-10-13 15:18:03 +0200

    Add swift-collections

--- Package.swift
+++ Package.swift
@@ -12,12 +12,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.67.0")),
+        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.1.0")),
     ],
     targets: [
         .target(
             name: "RabbitMQ",
             dependencies: [
-                .product(name: "NIOCore", package: "swift-nio")
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "Collections", package: "swift-collections")
             ]
         ),
         .testTarget(
