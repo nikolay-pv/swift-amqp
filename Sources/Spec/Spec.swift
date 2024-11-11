0b9ab3b257c16c1609419f0d0ef621ca35139b98
Nikolay Petrov <nikolay.a.petrov@gmail.com>
2024-11-11 22:11:33 +0100

    Mark AMQPObject sendable + add tests

--- Sources/Spec/Spec.swift
+++ Sources/Spec/Spec.swift
@@ -6,7 +6,7 @@
 
 import Foundation
 
-protocol AMQPObjectProtocol: Equatable {
+protocol AMQPObjectProtocol: Equatable, Sendable {
     var amqpName: String { get }
 }
 
---
+++ Tests/AMQPTests/Spec/CodableManual.swift
@@ -0,0 +1,31 @@
+//
+//  CodableManual.swift
+//  swift-amqp
+//
+//  Created by Nikolay Petrov on 11.11.2024.
+//
+
+import Foundation
+import Testing
+
+@testable import AMQP
+
+@Suite struct BoolPackCoding {
+    @Test(
+        "Sequential Bools are packed into one byte",
+        arguments: zip(
+            [
+                Spec.Basic.Consume(noLocal: true, noAck: true, exclusive: true, nowait: true),
+                Spec.Basic.Consume(noLocal: true, noAck: false, exclusive: true, nowait: true),
+                Spec.Basic.Consume(noLocal: false, noAck: true, exclusive: false, nowait: true),
+            ],
+            [0b1111, 0b1101, 0b1010]
+        )
+    )
+    func encodeSequenceOfBools(method: Spec.Basic.Consume, expectedByte: UInt8) async throws {
+        let encoded = try FrameEncoder().encode(method)
+        var expected = [UInt8].init(repeating: UInt8.zero, count: 9)
+        expected[4] = expectedByte
+        #expect(encoded == Data(expected))
+    }
+}
