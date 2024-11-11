461b1dd8f2b4a616857d1271e7b6d97bb612c13b
Nikolay Petrov <nikolay.a.petrov@gmail.com>
2024-11-11 21:43:48 +0100

    Rename tests for better grouping

--- Generator/run_generator.sh
+++ Generator/run_generator.sh
@@ -4,6 +4,6 @@ python $DIR/codegen.py 'header' $DIR/rabbitmq_codegen/amqp-rabbitmq-0.9.1.json $
 python $DIR/codegen.py 'body'   $DIR/rabbitmq_codegen/amqp-rabbitmq-0.9.1.json $DIR/../Sources/Spec/AMQP+AMQPCodable.swift
 # tests
 python $DIR/testgen.py 'header' $DIR/rabbitmq_codegen/amqp-rabbitmq-0.9.1.json $DIR/../Tests/AMQPTests/Spec/CodableRoundtrip.swift
-python $DIR/testgen.py 'body'   $DIR/rabbitmq_codegen/amqp-rabbitmq-0.9.1.json $DIR/../Tests/AMQPTests/Spec/VerifyEncoding.swift
+python $DIR/testgen.py 'body'   $DIR/rabbitmq_codegen/amqp-rabbitmq-0.9.1.json $DIR/../Tests/AMQPTests/Spec/CodableVerification.swift
 # format
-swift format format --in-place --configuration $DIR/../.swift-format $DIR/../Sources/Spec/AMQP.swift $DIR/../Sources/Spec/AMQP+AMQPCodable.swift $DIR/../Tests/AMQPTests/Spec/CodableRoundtrip.swift $DIR/../Tests/AMQPTests/Spec/VerifyEncoding.swift
+swift format format --in-place --configuration $DIR/../.swift-format $DIR/../Sources/Spec/AMQP.swift $DIR/../Sources/Spec/AMQP+AMQPCodable.swift $DIR/../Tests/AMQPTests/Spec/CodableRoundtrip.swift $DIR/../Tests/AMQPTests/Spec/CodableVerification.swift
